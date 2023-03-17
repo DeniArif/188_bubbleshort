c9cde980e9241cc15489ab10a4cccb4bd962a896
DeniArif <deniarifwibowo82@gmail.com>
2023-03-17 16:10:59 +0700

    DeniArif

--- Bubble sort/Bubble sort.cpp
+++ Bubble sort/Bubble sort.cpp
@@ -1,1 +1,57 @@
-// s
+#include <iostream>
+using namespace std;
+
+
+int a[20];
+int n;
+
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elmen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArry dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "====================" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
+}
+
+void display() {
+	cout << endl;
+	cout << "==================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "==================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+	cout << endl;
+}
+
+void bubbleSortArray() {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	input();
+	bubbleSortArray();
+	display();
+	return 0;
+}
